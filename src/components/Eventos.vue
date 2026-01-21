6c030e2875dad955853baa5abf7e99d91357b33c
gonzalofernandezri <gonzalo.fernandezr@elorrieta-errekamari.com>
2026-01-21 17:30:22 +0100

    Filtros mostrados y funcionando

--- src/components/Eventos.vue
+++ src/components/Eventos.vue
@@ -1,3 +1,33 @@
+<template>
+    <div>
+        <select v-model="tipo" @change="cargarEventos">
+            <option value="">Todos</option>
+            <option value="Presentación">Presentación</option>
+            <option value="Charla">Charla</option>
+            <option value="Taller">Taller</option>
+            <option value="Mesa Redonda">Mesa Redonda</option>
+            <option value="Exhibición">Exhibición</option>
+            <option value="Torneo">Torneo</option>
+            <option value="Networking">Networking</option>
+            <option value="Competición">Competición</option>
+        </select>
+
+        <ul>
+            <li v-for="evento in eventos" :key="evento.id">
+              <strong>{{ evento.titulo }}</strong><br>
+
+              Tipo: {{ evento.tipo }}<br>
+              Fecha: {{ evento.fecha }}<br>
+              Hora: {{ evento.hora }}<br>
+              Plazas libres: {{ evento.plazasLibres }}<br>
+              {{ evento.imagen }}<br>
+              Descripción: {{ evento.descripcion }}<br>
+          </li>
+        </ul>
+    </div>
+</template>
+
+
 <script setup>
 import { ref, onMounted } from 'vue'
 
@@ -18,18 +48,3 @@ async function cargarEventos() {
 onMounted(cargarEventos)
 </script>
 
-<template>
-    <div>
-        <select v-model="tipo" @change="cargarEventos">
-            <option value="">Todos</option>
-            <option value="Presentación">Presentación</option>
-            <option value="Charla">Charla</option>
-        </select>
-
-        <ul>
-            <li v-for="evento in eventos" :key="evento.id">
-            {{ evento.titulo }} — {{ evento.tipo }}
-            </li>
-        </ul>
-    </div>
-</template>
