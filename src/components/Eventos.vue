012a32113946c229ee2d486d87ae60ae8ddf3bcb
gonzalofernandezri <gonzalo.fernandezr@elorrieta-errekamari.com>
2026-01-22 19:11:56 +0100

    Paginacion de eventos funcional con filtros

--- src/components/Eventos.vue
+++ src/components/Eventos.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <select v-model="tipo" @change="cargarEventos">
+        <select v-model="tipo" @change="cambiarFiltros">
             <option value="">Todos</option>
             <option value="Presentación">Presentación</option>
             <option value="Charla">Charla</option>
@@ -18,10 +18,10 @@
           min="2025-02-13" 
           max="2025-02-15"
           v-model="fecha" 
-          @change="cargarEventos">
+          @change="cambiarFiltros">
 
         <label class="flex items-center gap-2 mt-2">
-          <input type="checkbox" v-model="soloConPlazas" @change="cargarEventos">
+          <input type="checkbox" v-model="soloConPlazas" @change="cambiarFiltros">
           Solo eventos con plazas disponibles
         </label>
 
@@ -36,18 +36,51 @@
               Descripción: {{ evento.descripcion }}<br>
           </li>
         </ul>
+
+        <button @click="anteriorPagina" :disabled="paginaActual === 1">
+          Anterior
+        </button>
+        
+        <button @click="siguientePagina" :disabled="paginaActual === totalPaginas">
+          Siguiente
+        </button> 
     </div>
 </template>
 
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted} from 'vue'
 
 const eventos = ref([])
 const tipo = ref('')
 const fecha = ref('')
 const soloConPlazas = ref(false)
 
+var paginaActual = 1
+var totalPaginas = 2
+const eventosPorPagina = 9
+
+async function calcularPaginas(){
+  const params = new URLSearchParams()
+  
+  if (tipo.value) {
+    params.append('tipo', tipo.value)
+  }
+
+  if (fecha.value) {
+    params.append('fecha', fecha.value)
+  }
+
+  if (soloConPlazas.value){
+    params.append('plazas', 'true')
+  } 
+
+  const res = await fetch(`/api/eventostodo_api.php?${params.toString()}`)
+  const data = await res.json();
+  console.log(data)
+  totalPaginas = Math.ceil(Number(data) / eventosPorPagina);
+  console.log(totalPaginas)
+}
 
 async function cargarEventos() {
   const params = new URLSearchParams()
@@ -64,10 +97,35 @@ async function cargarEventos() {
     params.append('plazas', 'true')
   } 
 
+  params.append('pagina', paginaActual)
+
   const res = await fetch(`/api/eventos_api.php?${params.toString()}`)
-  eventos.value = await res.json()
+  const data = await res.json()
+  eventos.value = data;
 }
 
-onMounted(cargarEventos)
+function cambiarFiltros() {
+  paginaActual = 1
+  calcularPaginas()
+  cargarEventos()
+}
+
+function siguientePagina() {
+  if (paginaActual < totalPaginas) {
+    paginaActual++
+    cargarEventos()
+  }
+}
+
+function anteriorPagina() {
+  if (paginaActual > 1) {
+    paginaActual--
+    cargarEventos()
+  }
+}
+onMounted(() => {
+  cargarEventos();
+  calcularPaginas();
+});
 </script>
 
