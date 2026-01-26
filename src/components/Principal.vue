3ef243775552e816ae397872465cb6d2f0a9c5c4
Enekoplaza2004 <eneko.plazava@elorrieta-errekamari.com>
2026-01-26 20:00:48 +0100

    Principal echo

--- src/components/Principal.vue
+++ src/components/Principal.vue
@@ -1,17 +1,48 @@
 <template>
-      <div class="">
-      
-      <h4 class="text-3xl font-bold">¡BIENVENIDO/A A NUESTRO FESTIVAL!</h4>
-      <p></p>
-      <!-- <router-link to="/Principal" class="text-red-400">No tienes cuenta creada? Registrate</router-link> -->
+  <div class="flex items-center justify-center min-h-screen bg-gray-100 fondo">
 
+    
+    <div class="w-100 h-70 bg-blue-400 text-white p-6 rounded-lg shadow-lg text-center space-y-20  back">
+      <h4 class="text-3xl font-bold">¡BIENVENIDO/A A ElorrietaFest!</h4>
+
+      <div class="flex flex-row justify-center gap-4 ">
+       <ol class="flex gap-6  ">
+          <li>
+            <router-link
+              to="/juegos"
+              class="bg-orange-300 rounded  text-black p-3 hover:text-orange-500 nav-link"
+            >
+              Juegos
+            </router-link>
+          </li>
+          <li>
+            <router-link
+              to="/eventos"
+              class="bg-orange-300 text-black rounded p-3 hover:text-orange-500 nav-link"
+            >
+              Eventos
+            </router-link>
+          </li>
+        </ol>
+      </div>
+      
     </div>
+  </div>
 </template>
 
 <script setup>
-
 </script>
 
 <style>
+
+.back{
+  background: linear-gradient(to right, var(--color-blue-500), var(--color-purple-400));
+}
+
+.fondo {
+
+  background-image: url('../../img/aaa.png');
+}
+
 
 </style>
