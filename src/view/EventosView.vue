94a2f61759268632286dfa3a3a5cb8f45ad986c2
Enekoplaza2004 <eneko.plazava@elorrieta-errekamari.com>
2026-01-28 19:08:12 +0100

    altura eventos

--- src/view/EventosView.vue
+++ src/view/EventosView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex items-center justify-center grow bg-gray-100">    
-      <div class="w-full max-w-6xl mx-auto p-6 flex flex-col items-center gap-6 pt-35">
+      <div class="w-full max-w-6xl mx-auto p-6 flex flex-col items-center gap-6 pt-40 md:pt-30">
         <div class="flex flex-col sm:flex-row gap-4 w-full">
           <select v-model="tipo" @change="cambiarFiltros"  class="border p-2 rounded flex-1">
             <option value="">Todos</option>
